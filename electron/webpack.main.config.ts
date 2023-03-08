1973405a6c27b4d072f1afe93901380ba9467171
Morgan Dilling <morgandilling171@gmail.com>
2023-03-08 21:20:20 +0000

    ci: fix

--- electron/webpack.main.config.ts
+++ electron/webpack.main.config.ts
@@ -1,5 +1,5 @@
 import type { Configuration } from 'webpack';
-import CopyPlugin from 'copy-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin';
 import { rules } from './webpack.rules';
 import path from 'path';
 
@@ -19,9 +19,17 @@ export const mainConfig: Configuration = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: path.resolve(__dirname, "src/renderer-app/**/*"), to: path.resolve(__dirname, '.webpack/renderer/main_window'), context: 'src/renderer-app/' },
-        { from: path.resolve(__dirname, "src/app/**/*"), to: path.resolve(__dirname, '.webpack/main/static'), context: 'src/app/' },
+        {
+          from: path.resolve(__dirname, 'src/renderer-app/'),
+          to: path.resolve(__dirname, '.webpack/renderer/main_window'),
+          context: 'src/renderer-app/',
+        },
+        {
+          from: path.resolve(__dirname, 'src/app/'),
+          to: path.resolve(__dirname, '.webpack/main/static'),
+          context: 'src/app/',
+        },
       ],
     }),
-  ]
+  ],
 };
