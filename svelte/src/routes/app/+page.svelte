d831f0c7ade46a49c874bd01b148ec6f73082fb4
Morgan Dilling <morgandilling171@gmail.com>
2023-03-10 21:20:01 +0000

    feat: temp app placeholder

--- svelte/src/routes/app/+page.svelte
+++ svelte/src/routes/app/+page.svelte
@@ -1,6 +1,19 @@
 <script lang="ts">
-  import { authedUser } from '@lib/databaseWrapper';
+  import { authedUser, logout } from '@lib/databaseWrapper';
+  import { goto } from '$app/navigation';
+
+  const Logout = () => {
+    logout();
+    goto('/register');
+  };
+
+  if ($authedUser?.userId === undefined) {
+    goto('/register');
+  }
 </script>
 
 <p>Signed in as user: {$authedUser?.userId}</p>
 <a href="/">home</a>
+<form>
+  <button type="submit" on:click={Logout}>Logout</button>
+</form>
