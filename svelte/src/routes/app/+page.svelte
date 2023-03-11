8ac74dc0c6b18f2932bebfc94035a90fa511be54
Morgan Dilling <morgandilling171@gmail.com>
2023-03-11 15:48:17 +0000

    feat: Temp app changes

--- svelte/src/routes/app/+page.svelte
+++ svelte/src/routes/app/+page.svelte
@@ -2,18 +2,37 @@
   import { authedUser, logout } from '@lib/databaseWrapper';
   import { goto } from '$app/navigation';
 
+  const user = localStorage.getItem('authedUser');
+
   const Logout = () => {
     logout();
     goto('/register');
   };
 
-  if ($authedUser?.userId === undefined) {
-    goto('/register');
-  }
+  if (user === 'null') goto('/register');
+
+  const privateKey = localStorage.getItem('privateKey');
 </script>
 
-<p>Signed in as user: {$authedUser?.userId}</p>
-<a href="/">home</a>
-<form>
-  <button type="submit" on:click={Logout}>Logout</button>
-</form>
+<main>
+  <p>Signed in as user: {$authedUser?.userId}</p>
+  <code>{privateKey}</code>
+  <br />
+
+  <a href="/">home</a>
+  <form>
+    <button type="submit" on:click={Logout}>Logout</button>
+  </form>
+</main>
+
+<style>
+  code {
+    display: block;
+    font-size: smaller;
+    word-wrap: break-word;
+    width: 450px;
+  }
+  main {
+    color: white;
+  }
+</style>
